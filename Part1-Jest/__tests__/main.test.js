b6696c3618824de75cae7b8e0fa88e4fe404bbad
sujanrai4 <sjn.bvet5@yahoo.com>
2021-02-24 16:45:55 -0800

    m

--- Part1-Jest/__tests__/main.test.js
+++ Part1-Jest/__tests__/main.test.js
@@ -1,36 +1,33 @@
 
-const formatVolumeIconPath = require('../assets/scripts/main');
+const { formatVolumeIconPath } = require('../assets/scripts/main');
 
-describe('volume icon level 3', () => {
-	test('volume level 3, upper bound', () => {
-		expect(formatVolumeIconPath(100)).toContain('3');
+describe('Icon level 3, greater than 66', () => {
+	test('Level 3, upper bound', () => {
+		expect(formatVolumeIconPath(100)).toMatch('3');
 	});
-	test('volume level 3, lower bound', () => {
-		expect(formatVolumeIconPath(67)).toContain('3');
-	});
-
-});
-describe('volume icon level 2', () => {
-	test('volume level 2, upper bound', () => {
-		expect(formatVolumeIconPath(66)).toContain('2');
-	});
-	test('volume level 2, lower bound', () => {
-		expect(formatVolumeIconPath(34)).toContain('2');
+	test('Level 3, lower bound', () => {
+		expect(formatVolumeIconPath(67)).toMatch('3');
 	});
 
 });
-describe('volume icon level 1', () => {
-	test('volume level 1, upper bound', () => {
-		expect(formatVolumeIconPath(33)).toContain('1');
+describe('Icon level 2, greater than 33 but less than 67', () => {
+	test('Level 2, upper bound', () => {
+		expect(formatVolumeIconPath(66)).toMatch('2');
 	});
-	test('volume level 1, lower bound', () => {
-		expect(formatVolumeIconPath(1)).toContain('1');
+	test('Level 2, lower bound', () => {
+		expect(formatVolumeIconPath(34)).toMatch('2');
 	});
 
 });
-describe('volume icon level 0', () => {
-	test('volume level 0, upper bound', () => {
-		expect(formatVolumeIconPath(0)).toContain('0');
+describe('Icon level 1, greater than 1 but less than 34', () => {
+	test('Level 1, upper bound', () => {
+		expect(formatVolumeIconPath(33)).toMatch('1');
+	});
+	test('Level 1, lower bound', () => {
+		expect(formatVolumeIconPath(1)).toMatch('1');
 	});
 
+});
+test('Icon level 0', () => {
+		expect(formatVolumeIconPath(0)).toMatch('0');
 });
