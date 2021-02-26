299dcaf4b196f6dba72a89802588a0e86e23f102
sujanrai4 <sjn.bvet5@yahoo.com>
2021-02-26 14:08:12 -0800

    m

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -1,6 +1,6 @@
 describe('Party Horn Tests', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5500/');
+    cy.visit('http://localhost:1932');
   });
 
   it('First Test', () => {
@@ -105,4 +105,89 @@ describe('Party Horn Tests', () => {
       expect($el).to.have.prop('paused', false);
     });
   });
+
+  it('Image and sound sources change when party-horn button is selected', () => {
+    cy.get('#radio-party-horn').click();
+    cy.get('#sound-image').then($el => {
+      expect($el).to.have.attr('src','./assets/media/images/party-horn.svg' );
+    });
+    cy.get('#horn-sound').then($el => {
+      expect($el).to.have.attr('src', './assets/media/audio/party-horn.mp3');
+    });
+  });
+
+  it('Volume image goes from 2 bars to 3 bars when the volume number goes from 66 to 67', () => {
+    cy.get('#volume-number')
+      .invoke('val', 66)
+      .trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/icons/volume-level-2.svg');
+    });
+    cy.get('#volume-number')
+      .invoke('val', 67)
+      .trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/icons/volume-level-3.svg');
+    });
+  });
+
+  it('Volume image goes from 1 bar to 2 bars when the volume number goes from 33 to 34', () => {
+    cy.get('#volume-number')
+      .invoke('val', 33)
+      .trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/icons/volume-level-1.svg');
+    });
+    cy.get('#volume-number')
+      .invoke('val', 34)
+      .trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/icons/volume-level-2.svg');
+    });
+  });
+    
+  it('Volume image goes from mute to 1 bar when the volume number goes from 0 to 1', () => {
+    cy.get('#volume-number')
+      .invoke('val', 0)
+      .trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/icons/volume-level-0.svg');
+    });
+    cy.get('#volume-number')
+      .invoke('val', 1)
+      .trigger('input');
+    cy.get('#volume-image').then($el => {
+      expect($el).to.have.attr('src', './assets/media/icons/volume-level-1.svg');
+    });
+  });
+
+  it('If honk button is disabled because of input is empty', () => {
+      cy.get('#volume-number').clear()
+      cy.get('#honk-btn').then($el => {
+        expect($el).to.have.attr('disabled');
+      });
+  });
+
+  it('If honk button is disabled because of non-number input', () => {
+      cy.get('#volume-number').clear().type('non-number')
+      cy.get('#honk-btn').then($el => {
+        expect($el).to.have.attr('disabled');
+      });
+  });
+
+  it('Error is shown when negative number entered as a volume input', () => {
+    cy.get('#volume-number').clear().type('-1')
+    cy.get('#honk-btn').click()
+    cy.get('input:invalid').then($el => {
+      expect($el[0].validationMessage).to.equal('Value must be greater than or equal to 0');
+    });
+  });
+
+  it('Error is shown when number >100 entered as a volume input', () => {
+    cy.get('#volume-number').clear().type('101')
+    cy.get('#honk-btn').click()
+    cy.get('input:invalid').then($el => {
+      expect($el[0].validationMessage).to.equal('Value must be less than or equal to 100');
+    });
+  }); 
 });
